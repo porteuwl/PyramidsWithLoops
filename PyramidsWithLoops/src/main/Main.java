77ab7f6bb53a1d26c5709168c390fec03c67b7a5
Luke <Luke@Luke.wifi.uc.edu>
2019-09-24 11:05:18 -0400

    Assignment 04 Finished

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,7 +1,11 @@
 /*
+ * Assignment 04
  * Luke Porteus
  * porteuwl@mail.uc.edu
- * Printing pyramids using loops. It kind-of works. 
+ * IS4010 App Dev with Java
+ * Fall 2019
+ * 09/24/2019
+ * Description: Printing pyramids using loops.
  * What I really want is
         1
        222
@@ -14,16 +18,16 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		  int rows = 5, k = 0;
-	        for(int i = 1; i <= rows; ++i, k = 0) {
-	            for(int space = 1; space <= rows - i; ++space) {
-	                System.out.print("  ");
+		  int rows = 5, k = 0;	//declares 5 rows
+	        for(int i = 1; i <= rows; ++i, k = 0) {	//if i is less than or equal to number  of row, add 2 more i's
+	            for(int space = 1; space <= rows - i; ++space) { // As rows go down, space is subtracted
+	                System.out.print(" "); // prints space
 	            }
-	            while(k != 2 * i - 1) {
-	                System.out.print("1 ");
-	                ++k;
+	            while(k != 2 * i - 1) { //start of loop
+	                System.out.print(i); //prints i
+	                ++k; //increments k by 1
 	            }
-	            System.out.println();
+	            System.out.println(); //prints loop
 	        }
 	    }
 	}
